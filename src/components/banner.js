cdef6b41b91f474594bfc9e14454570b8428bc26
elnakouzimohamed <nakouzim2004@gmail.com>
2025-03-06 14:06:48 +0200

    o

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,14 +4,14 @@ export default function Banner(){
     return(
 
         <section className="max-w-[1400px] m-auto   p-5">
-            <p className="text-white text-center  text-[82px]"> 
+            <p className="text-white text-center  md:text-[82px] sm:text-[50px] text-[30px]"> 
 
             here’s a teeny-weeny <br/>
 glimpse of our work. <br/>
 what’s coming later is <br/>
         
- <span className="text-[48px] text-blue-500 font-bold" >___</span><span className="text-red-400 text-[48px] font-bold ">_</span><span className="text-red-400 font-bold">even</span> greater
-            <span className="text-red-400 text-[48px] font-bold">___</span><span className="text-blue-500 text-[48px] font-bold">_ ____</span>
+ <span className=" text-blue-500 font-bold md:text-[48px] sm:text-[30px] text-[20px]" >___</span><span className="text-red-400 font-bold md:text-[48px] sm:text-[30px] text-[20px]">_</span><span className="text-red-400 font-bold">even</span> greater
+            <span className="text-red-400  font-bold md:text-[48px] sm:text-[30px] text-[20px]">___</span><span className="text-blue-500  font-bold md:text-[48px] sm:text-[30px] text-[20px]">_ ____</span>
             </p>
             </section>
       
