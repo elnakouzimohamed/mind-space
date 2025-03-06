95c9f91d65deb210dd2d28f79da2ce6a165cd7f8
elnakouzimohamed <nakouzim2004@gmail.com>
2025-03-06 13:10:24 +0200

    o

---
+++ src/components/Layout.js
@@ -0,0 +1,6 @@
+export default function Layout(){
+
+    return(
+        <section className="max-w-[1400px] bg-black" style={{backgroundImage: "url('/banner.svg')"}}></section>
+    )
+}
---
+++ src/components/banner.js
@@ -0,0 +1,20 @@
+export default function Banner(){
+
+
+    return(
+
+        <section className="max-w-[1400px] p-5">
+            <p className="text-white text-center  text-[82px]"> 
+
+            here’s a teeny-weeny <br/>
+glimpse of our work. <br/>
+what’s coming later is <br/>
+        
+ <span className="text-[48px] text-blue-500 font-bold" >___</span><span className="text-red-400 text-[48px] font-bold ">_</span><span className="text-red-400 font-bold">even</span> greater
+            <span className="text-red-400 text-[48px] font-bold">___</span><span className="text-blue-500 text-[48px] font-bold">_ ____</span>
+            </p>
+            </section>
+      
+    )
+
+}
