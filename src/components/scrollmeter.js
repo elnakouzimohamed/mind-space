ec879033860d5ec9c13a1ff578cfe28f21a5a7e5
elnakouzimohamed <nakouzim2004@gmail.com>
2025-03-07 13:46:27 +0200

    i

--- src/components/scrollmeter.js
+++ src/components/scrollmeter.js
@@ -8,7 +8,7 @@ const ScrollMeter = () => {
       const scrollTop = document.documentElement.scrollTop;
       const scrollHeight =
         document.documentElement.scrollHeight - document.documentElement.clientHeight;
-      const progress = 100 - (scrollTop / scrollHeight) * 100;
+      const progress =  (scrollTop / scrollHeight) * 100;
       setScrollProgress(progress);
     };
 
@@ -17,9 +17,9 @@ const ScrollMeter = () => {
   }, []);
 
   return (
-    <div className="fixed left-14 top-1/2 -translate-y-1/2 h-40 w-2 bg-red-500 rounded-full">
+    <div className="fixed left-14 top-1/2 -translate-y-1/2 h-40 w-2  bg-gray-300  rounded-full">
       <div
-        className="absolute bottom-0 w-full rounded-full  bg-gray-300  transition-all duration-150"
+        className="absolute top-0 w-full rounded-full  bg-red-500 transition-all duration-150"
         style={{ height: `${scrollProgress}%` }}
       ></div>
     </div>
